48343e60ce267ceda3a6cf06306e2132c665d6bc
Lise26 <lisecottin@gmail.com>
2019-11-19 12:49:17 +0100

    h

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <map>
+#include <string>
+using namespace std;
+
+#include "projet.cpp"
+#include "sequenceSearch.cpp"
+
+int main(int argc, char **argv) {
+	
+	//verif_parametres(argc, argv);
+	
+	cout << "Le programme commence" << endl;
+	ifstream proteinFile("./P00533.fasta");
+	
+	cout << "Appel à la fonction searchSequence" << endl;
+	searchSequence(proteinFile);
+	
+	cout << "Fermeture du fichier" << endl;
+	proteinFile.close();
+	
+	
+	return(0);
+}
