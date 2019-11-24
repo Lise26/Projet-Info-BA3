588953b21d934469d70a005e1fdc1f0f76c72c91
NataliaLeboutte <45312602+NataliaLeboutte@users.noreply.github.com>
2019-11-24 17:17:21 +0100

    Main incluant header

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 //#include "projet.cpp"
 #include "sequenceSearch.cpp"
 #include "index_reader.cpp"
+#include "headerSearch.cpp"
 
 int main(int argc, char **argv) {
 
@@ -21,13 +22,18 @@ int main(int argc, char **argv) {
 	vector<int> *tableau_offset(0);
 	cout << "après vector tableau" << endl;
 	
-	index_reader(tableau_offset);
+	vector<int> *header_offset(0);
+	
+	index_reader(tableau_offset,header_offset);  //sequence address in a table built from index File
 	cout << "après index_reader" << endl;
 	
 	ifstream proteinFile("./P00533.fasta");
 	
-	searchSequence(proteinFile, tableau_offset);
+	int indice = searchSequence(proteinFile, tableau_offset);  //renvoie adresse de la sequence correcte (ou 0 si aucune sesuence ne correspond à la protéine
 	cout << "après searchSequence" << endl;
+	
+	//string title = searchHeader(indice, header_offset);
+	//cout << "nom de la protéine: " << title << endl;
 	
 	proteinFile.close();
 	
