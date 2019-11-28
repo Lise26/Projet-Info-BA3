f9a8a18ddde4b9c79cfaec8a5cbc363c14d3e2f4
Lise26 <33690195+Lise26@users.noreply.github.com>
2019-11-28 22:23:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,84 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <map>
+#include <vector>
+#include <stdio.h>
 #include <string>
 using namespace std;
 
-#include "projet.cpp"
-#include "sequenceSearch.cpp"
+#include "parameters.h"
+#include "indexSearch.h"
+#include "sequenceSearch.h"
+#include "headerSearch.h"
 
 int main(int argc, char **argv) {
-
-	//verif_parametres(argc, argv);
 	
-	cout << "Le programme commence" << endl;
-	ifstream proteinFile("./P00533.fasta");
+	parameters verif;
+	indexSearch ind;
+	sequenceSearch s;
+	headerSearch head;
 	
-	cout << "Appel à la fonction searchSequence" << endl;
-	searchSequence(proteinFile);
+	verif.verifParameters(argc, argv);			//vérification of the number of parameters given in input
 	
-	cout << "Fermeture du fichier" << endl;
-
+	string nameSequence = argv[2];
+	nameSequence.append(".psq");
+	
+	string nameIndex = argv[2];
+	nameIndex.append(".pin");
+	
+	string nameHeader = argv[2];
+	nameHeader.append(".phr");
+	
+	ifstream proteinFile(argv[1]);									
+	ifstream sequenceFile(nameSequence);								
+	ifstream indexFile(nameIndex);                                   	
+	ifstream headerFile(nameHeader);
+	
+	vector<int> tableauOffset(0);
+	vector<int> headerOffset(0);
+	vector<char> titre;
+	vector<char> date;
+	vector<char> name;
+	int version, nbSeq, lmax, size, sizet, sized =0;
+	int64_t residu = 0;
+	
+	ind.indexReader(indexFile, &tableauOffset, &headerOffset, &version, &nbSeq, &lmax, &residu, &titre, &date, &sizet, &sized);		
+	
+	if(sequenceFile.is_open()) {
+		head.header_name(headerFile, s.sequenceMatch(proteinFile, sequenceFile, &tableauOffset), headerOffset, &name, &size); 
+	}
+	
+	sequenceFile.close();
 	proteinFile.close();
 	
+	string p(sizet,0);
+	for(int i = 0; i < sizet; i++){
+		p[i] = titre[i];
+	}
+	
+	string d(sized,0);
+	for(int i = 0; i < sized; i++){
+		d[i] = date[i];
+	}
+	
+	string n(size,0);
+	for(int i = 0; i < size; i++){
+		n[i] = name[i];
+	}
+
+	ofstream result("/home/student/Bureau/result.txt");
+	if(result){															//Avec les classes je vais arranger ça en plus beau, j'ai juste la flemme de le refaire 2x
+		result << "Database title : ";
+		result << p << endl;
+		result << "Database time : ";
+		result << d << endl;
+		result << "Database size : ";
+		result << residu << " residues in " << nbSeq << " sequences"<< endl;
+		result << "Longeset db sequence : ";
+		result << lmax << endl;
+		result << "\n\n" << endl;
+		result << "The matching protein is : \n" << n << endl;
+	}	
 	
 	return(0);
 }
