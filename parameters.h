d3afce3b1f6a8de77fc1115ff0cf7d9904aaa5b8
Lise26 <33690195+Lise26@users.noreply.github.com>
2019-11-28 22:24:36 +0100

    parameters.h

---
+++ parameters.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <stdio.h>
+using namespace std;
+
+class parameters {
+	
+	public:
+		int verifParameters(int argc, char **argv);
+};
