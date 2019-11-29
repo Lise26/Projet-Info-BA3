fbf833aeb9016fcc5343124135004295e33fea60
Lise26 <33690195+Lise26@users.noreply.github.com>
2019-11-29 17:09:53 +0100

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -1,13 +1,13 @@
 #include "conversion.h"
 
-string conversion::decToHex(int decimal){ 				//Convert from decimal to hex
+string conversion::decToHex(int decimal){ 				//Convert from decimal to hexadecimal
 	stringstream ss;
 	ss << hex << decimal;
 	string res ( ss.str() );
 	return res;
 }
 
-int conversion::hexToDecimal(string hex_value){				 //Convert from hex to decimal
+int conversion::hexToDecimal(string hex_value){				//Convert from hexadecimal to decimal
 	stringstream ss;
 	ss << hex_value;
 	int res;
