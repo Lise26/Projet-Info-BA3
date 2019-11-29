53fc43e7c91839e9d48c47f65c98b3f8f4e5fa4f
NataliaLeboutte <45312602+NataliaLeboutte@users.noreply.github.com>
2019-11-29 19:47:35 +0100

    + coms

--- sequenceSearch.cpp
+++ sequenceSearch.cpp
@@ -6,12 +6,12 @@ int sequenceSearch::sequenceMatch(ifstream &proteinFile, ifstream &sequenceFile,
 		
 	while(proteinFile >> protLetter and !proteinFile.eof()){  			//first condition puts each character of the proteinFile, one by one, into a variable protLetter
 																		//second condition : stop the loop when the end of the proteinFile is reached 
-		proteinTable.push_back(protLetter);	
+		proteinTable.push_back(protLetter);								//add each letter in a table
 		counter++;					
 	}
 
 	while(!sequenceFile.eof() and proteinFound == false){
-		offset = tableauOffset->at(it);
+		offset = tableauOffset->at(it);									//directly go to the beginning of a sequence in the sequence File
 		proteinFound = c.checkSequence(sequenceFile.seekg(offset), proteinTable);
 		it++;
 	}
